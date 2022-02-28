b8243e3a9fa2be551f2915ee39f0e903f02daf0e
yasinsunmaz <yasinsunmaz13@gmail.com>
2022-03-01 00:28:09 +0300

    Datan binding

--- src/app/components/property-binding/property-binding.component.ts
+++ src/app/components/property-binding/property-binding.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-binding.component.css']
 })
 export class PropertyBindingComponent implements OnInit {
-
+  numberOne: number = 1;
+  numberTwo: number = this.numberOne;
+  title: string = "<h1>Yasin SUNMAZ</h1>";
+  imgPath: string = "https://www.yasinsunmaz.com/ysadmin/ContentFiles/images/logo_14082018230718.jpg";
+  isOnline: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/string-interpolation/string-interpolation.component.ts
+++ src/app/components/string-interpolation/string-interpolation.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./string-interpolation.component.css']
 })
 export class StringInterpolationComponent implements OnInit {
+  //TODO: String Interpolation Kullanımı
+  title = 'String Interpolation Kullanımı';
+  //Matematiksel işlemler
+  number1: number = 3;
+  number2: number = 10;
 
+  firstValue: number = 3; 
+  secondValue: number = 10;
+  methodCall(): number {
+    return this.firstValue + this.secondValue;
+  };
+
+  name:string="Yasin";
+  surname:string="SUNMAZ";
+
+  users = {
+    username : 'yasinsunmaz',
+    password: 123
+  };
   constructor() { }
 
   ngOnInit(): void {
